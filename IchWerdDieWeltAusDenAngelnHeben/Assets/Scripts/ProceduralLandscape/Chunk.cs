872f46ee8e9e2a27adaf86c40fc91b853a556527
Markus Förste <markus_foerste@web.de>
2020-03-30 00:05:27 +0200

    Added serious changes

---
+++ IchWerdDieWeltAusDenAngelnHeben/Assets/Scripts/ProceduralLandscape/Chunk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // DAS MACHT GAR NICHTS XDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
